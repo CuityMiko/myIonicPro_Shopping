116575506ee759e65a68950f5719b6e507669d7e
CuityMiko <15201776956@163.com>
2017-05-25 11:53:56 +0800

    Tab修改

---
+++ www/areas/tab/controller.js
@@ -0,0 +1,7 @@
+/**
+ * tab控制器模块
+ */
+angular.module('starter.controller.tab',[])
+  .controller('TabCtrl',['$scope',function($scope){
+
+  }])
--- www/areas/tab/route.js
+++ www/areas/tab/route.js
@@ -4,6 +4,7 @@ angular.module('starter.tab',[
   'starter.route.tab_account',
   'starter.route.tab_chats',
   'starter.route.tab_chats_detail',
+  'starter.controller.tab',
   // 功能实现模块
   'starter.route.tab_home'
 ])
@@ -11,6 +12,7 @@ angular.module('starter.tab',[
     $stateProvider.state('tab', {
       url: '/tab',
       abstract: true, //抽象路由，跟着下面的路由匹配使用，eg:/tab/dash
-      templateUrl: 'areas/tab/tabs.html'
+      templateUrl: 'areas/tab/tabs.html',
+      controller:'TabCtrl'
     })
 }])
