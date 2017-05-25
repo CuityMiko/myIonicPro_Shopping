4fc9e4491185409caccd00fe2d030a416117ed09
CuityMiko <15201776956@163.com>
2017-05-25 13:04:03 +0800

    首页轮播swiper使用

--- www/areas/home/controller.js
+++ www/areas/home/controller.js
@@ -2,6 +2,25 @@
  * 首页控制器模块
  */
 angular.module('starter.controller.tab_home',['starter.service.tab_home'])
-  .controller('HomeCtrl',['$scope',function($scope){
-
+  .controller('HomeCtrl',['$scope','HomeFcty',function($scope,HomeFcty){
+    //获取swiper动态加载的数据
+    $scope.HeaderSlideData=HomeFcty.getHeaderSlideData();
+    //监听ion-view组件视图完全加载之后的事件
+    $scope.$on("$ionicView.afterEnter",(e)=>{
+      //swiper引导页
+      var mySwiper = new Swiper ('.swiper-container', {
+        slidesPerView:1, //在slider的前后各添加一张相同的slider
+        //direction: 'vertical', //纵向滚动，默认横向
+        loop: true, //循环切换
+        autoplay:3000, //自动切换间隔时间
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+        paginationClickable:true, //分页器是否可点
+        observer:true, //当动态添加swiper的时候，动态初始化swiper
+        observeParents:true
+        // onSlideChangeEnd:function(swiper){ //滑动到下一个页面触发事件
+        //   mySwiper.activeIndex //滑动到第几个页面
+        // }
+      })
+    })
   }])
--- www/areas/home/service.js
+++ www/areas/home/service.js
@@ -3,5 +3,29 @@
  */
 angular.module('starter.service.tab_home',[])
   .factory('HomeFcty', function() {
-    return null;
+    var HeaderSlideData=[
+        {
+          alt:"双十一预热主场会",
+          src:"img/home/home-headerSlide-1.jpg"
+        },
+        {
+          alt:"11月11天家电低价不停歇",
+          src:"img/home/home-headerSlide-2.jpg"
+        },
+        {
+          alt:"家具盛典 好货提前抢",
+          src:"img/home/home-headerSlide-3.jpg"
+        },
+        {
+          alt:"IT抢券节",
+          src:"img/home/home-headerSlide-4.jpg"
+        },
+        {
+          alt:"潮流数码 双11爽购攻略",
+          src:"img/home/home-headerSlide-5.jpg"
+        }
+      ];
+    return {
+      getHeaderSlideData:()=>{return HeaderSlideData;}
+    };
   });
