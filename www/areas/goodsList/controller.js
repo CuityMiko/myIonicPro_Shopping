6e423222566638d73e1de85abb13261d769c6ace
CuityMiko <15201776956@163.com>
2017-05-27 10:50:36 +0800

    ion-infinite-scroll和ion-refresher组件的使用

--- www/areas/goodsList/controller.js
+++ www/areas/goodsList/controller.js
@@ -1,4 +1,56 @@
 angular.module('starter.controller.goodsList',['starter.service.goodsList'])
-  .controller('GoodsListCtrl',['$scope',function($scope){
-    console.log(1111)
+  .controller('GoodsListCtrl',['$scope','GoodsListFcty',function($scope,GoodsListFcty){
+    // var flag=1;
+    // var gdpromise= GoodsListFcty.getdata(flag);
+    // gdpromise.then(
+    // (data)=>{ //成功回调
+    //   $scope.gdata=data;
+    // },
+    // (err)=>{ //失败
+    //   console.log(err);
+    // });
+
+
+    $scope.gdata=[];
+    $scope.pageindex=1;
+    $scope.isloadmore=false;
+    var getpagedata=function(flag){
+      var gdpromise= GoodsListFcty.getpagedata($scope.pageindex);
+      gdpromise.then((data)=>{
+        if(data==null || data.length<=0){
+          $scope.isloadmore=false;
+          return;
+        }
+        if(flag>0){
+          data.forEach((item)=>{
+            $scope.gdata.push(item)
+          })
+        }else
+          $scope.gdata=data;
+      }).finally(function() {
+        if(flag<1){
+          // 停止广播ion-refresher
+          $scope.$broadcast('scroll.refreshComplete');
+          $scope.isloadmore=true;
+        }
+        else
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+     });
+    }
+    //ion-view刚刚加载完毕的时候
+    $scope.$on('$ionicView.beforeEnter',function(){
+      getpagedata(0);
+    })
+    //下拉刷新
+    $scope.func_refreshGoodsList=function(){
+      $scope.pageindex=1;
+      $scope.gdata=[];
+      $scope.isloadmore=false;
+      getpagedata(0);
+    }
+    //上拉加载更多
+    $scope.loadMoreData=function(){
+      $scope.pageindex++;
+      getpagedata(1);
+    }
   }])
--- www/areas/goodsList/service.js
+++ www/areas/goodsList/service.js
@@ -1,4 +1,40 @@
 angular.module('starter.service.goodsList',[])
-  .factory('goodsListFcty',function(){
-    return null;
-  })
+  .factory('GoodsListFcty',['$q','$timeout',function($q,$timeout){
+    var getgoodsdata=function(){
+      var goodsdata=[];
+      for (var i = 0; i < 100; i++) {
+        goodsdata.push({
+          id:i+1,
+          name:`商品${i+1}`
+        });
+      }
+      return goodsdata;
+    }
+    var getpagedata=function(pageindex){
+      return getgoodsdata().splice((pageindex-1)*15,15);
+    }
+    return {
+      getdata:(flag)=>{
+        //$q的promise方法
+        var deferred=$q.defer();
+        $timeout(()=>{
+          if(flag>0)
+            deferred.resolve(getgoodsdata()) //成功
+          else
+            deferred.reject('数据有误') //失败
+        },3000);
+        return deferred.promise;
+      },
+      getpagedata:(pageindex)=>{
+        var deferred=$q.defer();
+        var timer=1500;
+        if(pageindex==1){
+            timer=5000;
+        }
+        $timeout(()=>{
+          deferred.resolve(getpagedata(pageindex));
+        },timer)
+        return deferred.promise;
+      }
+    }
+  }])
